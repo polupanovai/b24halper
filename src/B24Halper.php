f00978cf45fcea3e2900f1ebcffffe245b7737e2
root <root@vm-5858ab06.netangels.ru>
2020-02-14 11:10:07 +0000

    First composer commit

---
+++ src/B24Halper.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class B24Halper
+{
+    public function __construct()
+    {
+
+    }
+
+    /* Инициализация */
+    public function init()
+    {
+        return 'hallow wolrd';
+    }
+
+}
